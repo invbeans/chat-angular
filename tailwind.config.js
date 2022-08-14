1f4d86fc2075879240abf5c047a770de2492f09a
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-08-14 23:06:54 +0400

    params for tools

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'friend-list': '#5cb8b3',
+        'friend-block': '#499995',
+        'nav-div': '#4f807d',
+        'global-bg': '#486b69',
+        'dialog-bg': '#93c9c6',
+        'other-message': '#529c7c',
+        'self-message': '#989c52'
+
+      }
+    },
   },
   plugins: [],
 };
