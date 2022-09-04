a95ce30718b9bf9c2688cef63f502ebb2e34f6bb
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-09-04 23:57:37 +0400

    modules imports

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -4,16 +4,21 @@ import { MainPageComponent } from './main-page.component';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { SelfProfileComponent } from './self-profile/self-profile.component';
 import { SharedModule } from '../shared/shared.module';
+import { EditProfileComponent } from './edit-profile/edit-profile/edit-profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     MainPageRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     MainPageComponent,
-    SelfProfileComponent
+    SelfProfileComponent,
+    EditProfileComponent
   ]
 })
 export class MainPageModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './components/modal/modal.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 import { ScrollBarComponent } from './components/scroll-bar/scroll-bar.component';
 import { FocusDirective } from './directives/focus.directive';
 import { ModalService } from './services/modal.service';
-import { AppRoutingModule } from '../app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
