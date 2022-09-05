97423c793246efb7c1331279e0e9086d2a3f2448
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-09-06 02:31:11 +0400

    working scrolling in chats

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './components/modal/modal.component';
-import { ScrollBarComponent } from './components/scroll-bar/scroll-bar.component';
 import { FocusDirective } from './directives/focus.directive';
 import { ModalService } from './services/modal.service';
 
@@ -11,7 +10,6 @@ import { ModalService } from './services/modal.service';
   ],
   declarations: [
     ModalComponent,
-    ScrollBarComponent,
     FocusDirective
   ],
   providers: [
@@ -19,7 +17,6 @@ import { ModalService } from './services/modal.service';
   ],
   exports: [
     ModalComponent,
-    ScrollBarComponent,
     FocusDirective
   ]
 })
