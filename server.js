581ac040539afc8fc43c108d23d7501f309d1245
Vasileva Vlada <vlada.3234.vasileva@gmail.com>
2022-09-15 01:48:13 +0400

    .

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/chat-project'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/chat-project/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/chat-project/index.html'));});
 app.listen(process.env.PORT || 8080);
